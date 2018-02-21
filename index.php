d220b67acbbaa721d05a858df14f3e301524a432
bruckbmrj <bruckbmrj@hotmail.com>
2018-02-21 00:54:18 -0300

    tela de login

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php 
-
+session_start();
 require_once("vendor/autoload.php");
 
 use \Slim\Slim;
@@ -21,6 +21,8 @@ $app->get('/', function() {
 
 $app->get('/admin', function() {
     
+    User::verifyLogin();
+
 	$page = new PageAdmin();
 
 	$page->setTpl("index");
@@ -42,8 +44,16 @@ $app->post('/admin/login', function() {
     
 	User::login($_POST["login"], $_POST["password"]);
 
-	header("location:/admin");
+	header("location: /admin");
+	exit;
+});
 
+$app->get('/admin/logout', function(){
+
+	User::logout();
+
+	header("Location: /admin/login");
+	exit;
 });
 
 $app->run();
--- vendor/hcodebr/php-classes/src/Model/User.php
+++ vendor/hcodebr/php-classes/src/Model/User.php
@@ -6,13 +6,15 @@ use \Hcode\DB\Sql;
 use \Hcode\Model;
 
 class User extends Model{
+
+	const SESSION = "User";
 	
 	public static function login($login, $password)
 	{
 		
 		$sql = new Sql();
 
-		$results = $sql->select("SELECT * FROM tb_user WHERE deslogin = :LOGIN", array(
+		$results = $sql->select("SELECT * FROM tb_users WHERE deslogin = :LOGIN", array(
 			":LOGIN"=>$login
 		));
 
@@ -25,7 +27,11 @@ class User extends Model{
 		if(password_verify($password, $data["despassword"]) === true ){
 			$user = new User();
 
-			$user->setiduser($data["iduser"]);
+			$user->setData($data);
+
+			$_SESSION[User::SESSION] = $user->getValues();
+
+			return $user;
 
 
 		} else{
@@ -33,6 +39,30 @@ class User extends Model{
 		}
 
 	}
+
+	public static function verifyLogin($inadmin = true){
+
+		if(
+			!isset($_SESSION[User::SESSION])
+			||
+			!$_SESSION[User::SESSION]
+			||
+			!(int)$_SESSION[User::SESSION]["iduser"] > 0
+			||
+			(bool)$_SESSION[User::SESSION]["inadmin"] !== $inadmin	
+		) {
+			header("Location: /admin/login");
+			exit;
+		}
+
+	}
+
+	public static function logout(){
+
+		$_SESSION[User::SESSION] = NULL;
+		
+	}
+
 }
 
  ?>
