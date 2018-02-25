894b7aad7a674b8101c3a88b6aa1b879db0961a8
bruckbmrj <bruckbmrj@hotmail.com>
2018-02-25 19:12:18 -0300

    esqueci senha

--- vendor/hcodebr/php-classes/src/Mailer.php
+++ vendor/hcodebr/php-classes/src/Mailer.php
@@ -6,8 +6,8 @@ use Rain\Tpl;
 
 class Mailer{
 
-	const USERNAME = "bruckrj@gmail.com";
-	const PASSWORD = "bbmfla";
+	const USERNAME = "";
+	const PASSWORD = "";
 	const NAME_FROM = "Bruno Braga";
 
 	private $mail;
