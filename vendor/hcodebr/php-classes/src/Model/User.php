e7f0126401e7224ffa6dfe35877d60584921b7ca
bruckbmrj <bruckbmrj@hotmail.com>
2018-04-04 00:06:41 -0300

    Site cadastro

--- vendor/hcodebr/php-classes/src/Model/User.php
+++ vendor/hcodebr/php-classes/src/Model/User.php
@@ -11,7 +11,7 @@ class User extends Model{
 	const SESSION = "User";
 	const SECRET = "HcodePhp7_Secret";
 	const ERROR = "UserError";
-	//const ERROR_REGISTER = "UserErrorRegister";
+	const ERROR_REGISTER = "UserErrorRegister";
 
 	public static function getFromSession(){
 
@@ -76,7 +76,7 @@ class User extends Model{
 		if(password_verify($password, $data["despassword"]) === true ){
 			$user = new User();
 
-			$data['desperson'] = utf8_encode($data['desperson']);
+			
 
 			$user->setData($data);
 
@@ -149,7 +149,7 @@ class User extends Model{
 
 		$data = $results[0];
 
-		$data['desperson'] = utf8_encode($data['desperson']);
+		
 
 		$this->setData($data);
 
@@ -162,7 +162,7 @@ class User extends Model{
 		$results = $sql->select( "CALL sp_usersupdate_save(:iduser, :desperson, :deslogin, :despassword, :desemail, :nrphone, :inadmin)", array(
 		
 			":iduser"=>$this->getiduser(),
-			":desperson"=>utf8_decode($this->getdesperson()),
+			":desperson"=>$this->getdesperson(),
 			":deslogin"=>$this->getdeslogin(),
 			":despassword"=>$this->getdespassword(),
 			":desemail"=>$this->getdesemail(),
@@ -304,6 +304,40 @@ class User extends Model{
 
 	}
 
+
+	public static function setErrorRegister($msg){
+
+		$_SESSION[User::ERROR_REGISTER] = $msg;
+
+	}
+
+	public static function getErrorRegister(){
+
+		$msg = (isset($_SESSION[User::ERROR_REGISTER]) && $_SESSION[User::ERROR_REGISTER]) ? $_SESSION[User::ERROR_REGISTER] : '';
+
+		User::clearErrorRegister();
+
+		return $msg;
+
+	}
+
+	public static function clearErrorRegister(){
+
+		$_SESSION[User::ERROR_REGISTER] = NULL;
+
+	}
+
+	public static function checkLoginExists($login){
+
+		$sql = new Sql();
+
+		$results = $sql->select("SELECT * FROM tb_users WHERE deslogin = :deslogin", [
+			':deslogin'=>$login
+		]);
+
+		return (count($results) > 0);
+	}
+
 	public static function getPasswordHash($password){
 
 		return password_hash($password, PASSWORD_DEFAULT, [
@@ -312,5 +346,6 @@ class User extends Model{
 	}
 
 }
+
 
  ?>
--- views-cache/header.b18aae8b7bfe6bd31c1fe7cf29d9e8b3.rtpl.php
+++ views-cache/header.b18aae8b7bfe6bd31c1fe7cf29d9e8b3.rtpl.php
@@ -5,7 +5,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
 -->
 <html>
 <head>
-  <meta charset="utf-8">
+  <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <title>AdminLTE 2 | Starter</title>
   <!-- Tell the browser to be responsive to screen width -->
